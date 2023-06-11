23b183f569e1447a0da890eece9a4f192c83e08e
Rhine Cliff <rhinecliff@protonmail.com>
2023-06-11 11:58:05 -0700

    Update Gitlab CICD to test GCP Workload Identity Federation

---
+++ scripts/call_gcp_sts.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -x
+
+PAYLOAD="$(cat <<EOF
+{
+  "audience": "//iam.googleapis.com/projects/762069519666/locations/global/workloadIdentityPools/cicd-workflow/providers/gitlab",
+  "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
+  "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
+  "scope": "https://www.googleapis.com/auth/cloud-platform",
+  "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
+  "subjectToken": "${GITLAB_OIDC_TOKEN}"
+}
+EOF
+)"
+
+FEDERATED_TOKEN="$(curl --fail "https://sts.googleapis.com/v1/token" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --data "${PAYLOAD}" \
+  | jq -r '.access_token'
+)"
+
+ACCESS_TOKEN="$(curl --fail "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/pubsub-dev@cicd-389504.iam.gserviceaccount.com:generateAccessToken" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${FEDERATED_TOKEN}" \
+  --data '{"scope": ["https://www.googleapis.com/auth/cloud-platform"]}' \
+  | jq -r '.accessToken'
+)"
+echo "${ACCESS_TOKEN}"
