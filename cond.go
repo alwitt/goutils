4bdac579811674af934a8924efeeaf45a6b595ae
Rhine Cliff <rhinecliff@protonmail.com>
2025-04-27 22:36:32 -0400

    patch[sync]: Add Condition Variable
    Define custom condition variable with behavior similar `std::condition_variable` from C++.

---
+++ cond.go
@@ -0,0 +1,125 @@
+package goutils
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// Condition variable which has similar behavior to the C++11 std::condition_variable
+type Condition interface {
+	// NotifyOne notify one waiter
+	NotifyOne() error
+
+	// NotifyAll notify all waiters
+	NotifyAll() error
+
+	/*
+		Wait caller will block and wait to be notified.
+
+		The signaling channel is provided by the caller, and allows the same channel to be reused
+		for subsequent calls.
+
+		IMPORTANT: the signaling channel must be buffered
+
+			@param ctx context.Context - calling context
+			@param wakeUp chan bool - signaling channel
+	*/
+	Wait(ctx context.Context, wakeUp chan bool) error
+}
+
+// conditionImpl implement Condition
+type conditionImpl struct {
+	waitersLock   sync.Mutex
+	waiters       map[uint64]chan bool
+	waiterIDTrack atomic.Uint64
+}
+
+/*
+NewCondition get new condition variable.
+
+This condition has similar behavior to the C++11 std::condition_variable.
+
+	@return new condition variable
+*/
+func NewCondition() Condition {
+	return &conditionImpl{
+		waitersLock:   sync.Mutex{},
+		waiters:       make(map[uint64]chan bool),
+		waiterIDTrack: atomic.Uint64{},
+	}
+}
+
+// notify core notify function
+func (c *conditionImpl) notify(allWaiters bool) error {
+	c.waitersLock.Lock()
+	defer c.waitersLock.Unlock()
+
+	for waiterID, signalChan := range c.waiters {
+		// Notify waiter
+		signalChan <- true
+		// Remove waiter from list
+		delete(c.waiters, waiterID)
+
+		// Channel remains open, which allows it to be re-used again
+
+		if !allWaiters {
+			break
+		}
+	}
+
+	return nil
+}
+
+// NotifyOne notify one waiter
+func (c *conditionImpl) NotifyOne() error {
+	return c.notify(false)
+}
+
+// NotifyAll notify all waiters
+func (c *conditionImpl) NotifyAll() error {
+	return c.notify(true)
+}
+
+/*
+Wait caller will block and wait to be notified.
+
+The signaling channel is provided by the caller, and allows the same channel to be reused
+for subsequent calls.
+
+IMPORTANT: the signaling channel must be buffered
+
+	@param ctx context.Context - calling context
+	@param wakeUp chan bool - signaling channel
+*/
+func (c *conditionImpl) Wait(ctx context.Context, wakeUp chan bool) error {
+	waiterID := c.waiterIDTrack.Add(1)
+
+	// Helper function to insert caller into waiting pool
+	register := func() {
+		c.waitersLock.Lock()
+		defer c.waitersLock.Unlock()
+		c.waiters[waiterID] = wakeUp
+	}
+
+	// Helper function to remove caller from waiting pool
+	deregister := func() {
+		c.waitersLock.Lock()
+		defer c.waitersLock.Unlock()
+		delete(c.waiters, waiterID)
+	}
+
+	// register for signal
+	register()
+
+	// wait
+	select {
+	case <-ctx.Done():
+		deregister()
+		return fmt.Errorf("wait timed out")
+	case <-wakeUp:
+	}
+
+	return nil
+}
---
+++ cond_test.go
@@ -0,0 +1,171 @@
+package goutils
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/apex/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConditionNotifyOne(t *testing.T) {
+	assert := assert.New(t)
+	log.SetLevel(log.DebugLevel)
+
+	utCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	uut := NewCondition()
+
+	// Case 0: no waiters
+	{
+		assert.NotPanics(func() { assert.Nil(uut.NotifyOne()) })
+		assert.NotPanics(func() { assert.Nil(uut.NotifyAll()) })
+	}
+
+	testSignalChan := make(chan bool, 1)
+
+	// Case 1: one waiter
+	{
+		complete := make(chan bool, 1)
+
+		lclCtx, lclCancel := context.WithTimeout(utCtx, time.Millisecond*20)
+
+		go func() {
+			assert.Nil(uut.Wait(lclCtx, testSignalChan))
+			complete <- true
+		}()
+
+		time.Sleep(time.Millisecond * 5)
+		assert.Nil(uut.NotifyOne())
+
+		select {
+		case <-lclCtx.Done():
+			assert.False(true, "wait timed out")
+		case <-complete:
+		}
+		lclCancel()
+	}
+
+	// Case 2: one waiter timeout
+	{
+		complete := make(chan bool, 1)
+
+		lclCtx, lclCancel := context.WithTimeout(utCtx, time.Millisecond*20)
+
+		go func() {
+			waitCtx, waitCancel := context.WithTimeout(utCtx, time.Millisecond*10)
+			defer waitCancel()
+			assert.NotNil(uut.Wait(waitCtx, testSignalChan))
+			complete <- true
+		}()
+
+		select {
+		case <-lclCtx.Done():
+			assert.False(true, "wait timed out")
+		case <-complete:
+		}
+		lclCancel()
+	}
+
+	// Case 3: sequentially notify multiple waiters
+	{
+		waiters := 3
+		complete := make(chan int, waiters)
+
+		lclCtx, lclCancel := context.WithTimeout(utCtx, time.Millisecond*20)
+
+		// Helper function to run the wait
+		wg := sync.WaitGroup{}
+		wait := func(id int) {
+			waitCtx, waitCancel := context.WithTimeout(utCtx, time.Millisecond*10)
+			defer waitCancel()
+			defer wg.Done()
+			signal := make(chan bool, 1)
+			assert.Nil(uut.Wait(waitCtx, signal))
+			complete <- id
+		}
+
+		// Wait
+		for itr := 0; itr < waiters; itr++ {
+			wg.Add(1)
+			go wait(itr)
+		}
+
+		time.Sleep(time.Millisecond * 5)
+		// notify one at a time
+		for itr := 0; itr < waiters; itr++ {
+			assert.Nil(uut.NotifyOne())
+		}
+
+		assert.Nil(TimeBoundedWaitGroupWait(lclCtx, &wg, time.Millisecond*20))
+
+		waited := map[int]bool{}
+		for itr := 0; itr < waiters; itr++ {
+			select {
+			case <-lclCtx.Done():
+				assert.False(true, "wait timed out")
+			case id, ok := <-complete:
+				assert.True(ok)
+				waited[id] = true
+			}
+		}
+		assert.Len(waited, waiters)
+
+		lclCancel()
+	}
+}
+
+func TestConditionNotifyAll(t *testing.T) {
+	assert := assert.New(t)
+	log.SetLevel(log.DebugLevel)
+
+	utCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	uut := NewCondition()
+
+	waiters := 5
+	complete := make(chan int, waiters)
+
+	lclCtx, lclCancel := context.WithTimeout(utCtx, time.Millisecond*20)
+
+	// Helper function to run the wait
+	wg := sync.WaitGroup{}
+	wait := func(id int) {
+		waitCtx, waitCancel := context.WithTimeout(utCtx, time.Millisecond*10)
+		defer waitCancel()
+		defer wg.Done()
+		signal := make(chan bool, 1)
+		assert.Nil(uut.Wait(waitCtx, signal))
+		complete <- id
+	}
+
+	// Wait
+	for itr := 0; itr < waiters; itr++ {
+		wg.Add(1)
+		go wait(itr)
+	}
+
+	// Notify waiters
+	time.Sleep(time.Millisecond * 5)
+	assert.Nil(uut.NotifyAll())
+
+	assert.Nil(TimeBoundedWaitGroupWait(lclCtx, &wg, time.Millisecond*20))
+
+	waited := map[int]bool{}
+	for itr := 0; itr < waiters; itr++ {
+		select {
+		case <-lclCtx.Done():
+			assert.False(true, "wait timed out")
+		case id, ok := <-complete:
+			assert.True(ok)
+			waited[id] = true
+		}
+	}
+	assert.Len(waited, waiters)
+
+	lclCancel()
+}
